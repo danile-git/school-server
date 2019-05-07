ebf676f675993ba10b8406cc37f7c55585c166c6
danile-git <shaobodou@hotmail.com>
2019-05-07 17:59:22 +0800

    update

--- school-manager/school-manager-service/src/main/java/com/school/cc/service/impl/NewsServiceImpl.java
+++ school-manager/school-manager-service/src/main/java/com/school/cc/service/impl/NewsServiceImpl.java
@@ -1,10 +1,13 @@
 package com.school.cc.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.school.cc.mapper.NewsMapper;
 import com.school.cc.pojo.News;
+import com.school.cc.pojo.NewsExample;
 import com.school.cc.service.inter.NewsService;
 
 @Service
@@ -16,6 +19,14 @@ public class NewsServiceImpl implements NewsService {
 	public boolean insert(News news) {
 		boolean flg = newsMapper.insertSelective(news) > 0 ? true : false;
 		return flg;
+	}
+
+	@Override
+	public List<News> select(int index, int count) {
+		NewsExample newsExample=new NewsExample();
+		
+		newsMapper.selectByExample(newsExample);
+		return null;
 	}
 
 }
--- school-manager/school-manager-service/src/main/java/com/school/cc/service/inter/NewsService.java
+++ school-manager/school-manager-service/src/main/java/com/school/cc/service/inter/NewsService.java
@@ -1,7 +1,10 @@
 package com.school.cc.service.inter;
 
+import java.util.List;
+
 import com.school.cc.pojo.News;
 
 public interface NewsService {
 	boolean insert(News news);
+	List<News> select(int index,int count);
 }
